277cd698b9128e0d226a2e9f912dafc382e2ecd4
Bharat Singh <111725207+barrett28@users.noreply.github.com>
2024-11-24 14:38:26 +0530

    file added

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const getColor = ()  => {
+    //Hex Code 
+    const randomNumber= Math.floor(Math.random() * 16777215);
+    const randomcode = '#'+ randomNumber.toString(16);
+    console.log( randomNumber, randomcode);
+    document.body.style.backgroundColor = randomcode;
+    document.getElementById('color-code').innerText = randomcode;
+}
+//event call
+document.getElementById('btn').addEventListener(
+    'click',
+    getColor
+)
+getColor();
